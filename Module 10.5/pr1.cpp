4aa902aa719c77dde28ca40b1fd5cf42cb141cb6
Tahsin005 <tahsin.ferdous3546@gmail.com>
2023-08-15 02:53:07 +0600

    algo

---
+++ Module 10.5/pr1.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define Faster ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+const int N = 1e5 + 10;
+typedef pair<int,int> pii;
+vector<pair<int,int>> adj[N];
+bool visited[N];
+vector<int> dist(N,INT_MAX);
+void dijkstra(int source){
+    priority_queue<pii,vector<pii>,greater<pii>> pq; 
+    dist[source] = 0;
+    pq.push({dist[source],source});
+
+    while(!pq.empty()){
+        int u = pq.top().second;
+        pq.pop();
+        visited[u] = true;
+
+        for(auto p:adj[u]){
+            int v = p.first;
+            int w = p.second;
+            
+            if(visited[v]) continue;
+            if(dist[v] > dist[u] + w){
+                dist[v] = dist[u] + w;
+                pq.push({dist[v],v});
+            }
+        }
+
+    }
+}
+int main(){
+    Faster
+    int n,m;
+    cin>>n>>m;
+    for(int i = 0; i < m; i++){
+        int u,v,w;
+        cin>>u>>v>>w;
+        adj[u].push_back({v,w});
+        adj[v].push_back({u,w});
+    }
+    int s,d;
+    cin>>s>>d;
+    dijkstra(s);
+    cout<<dist[d]<<endl;
+    
+    return 0;
+}
