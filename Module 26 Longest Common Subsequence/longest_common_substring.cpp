697fd991c34076c37378a4572e7578de7b148d72
Tahsin005 <tahsin.ferdous3546@gmail.com>
2023-09-11 12:55:11 +0600

    algo

---
+++ Module 26 Longest Common Subsequence/longest_common_substring.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define Tahsin ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+int main(){
+    Tahsin
+    string a,b;
+    cin>>a>>b;
+    int n = a.size();
+    int m = b.size();
+    int dp[n + 1][m + 1];
+    for(int i = 0; i <= n; i++){
+        for(int j = 0; j <= m; j++){
+            if(i == 0 or j == 0) dp[i][j] = 0;
+        }
+    }
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            if(a[i - 1] == b[j - 1]){
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            }
+            else{
+                dp[i][j] = 0;
+            }
+        }
+    }
+    int mx = INT_MIN;
+    int ci,cj;
+    for(int i = 0; i <= n; i++){
+        for(int j = 0; j <= m; j++){
+            if(dp[i][j] > mx){
+                mx = dp[i][j];
+                ci = i,cj = j;
+            }
+            // cout<<dp[i][j]<<" ";
+        }
+        // cout<<endl;
+    }
+    // cout<<mx<<endl;
+    string ans;
+    while(ci != 0 and cj != 0){
+        if(a[ci - 1] == b[cj - 1]){
+            ans += a[ci - 1];
+            ci--;
+            cj--;
+        }
+        else{
+            break;
+        }
+    }
+    reverse(ans.begin(),ans.end());
+    cout<<ans<<endl;
+    return 0;
+}
