75e9b26c7406c56f6a1c4481458f51ed08fc69e6
Tahsin005 <tahsin.ferdous3546@gmail.com>
2023-09-12 01:55:48 +0600

    algo

--- Module 26 Longest Common Subsequence/lcs_top_down.cpp
+++ Module 26 Longest Common Subsequence/lcs_top_down.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int dp[1005][1005];
 int lcs(string a,int n,string b,int m){
     if(n == 0 or m == 0) return 0;
+    if(dp[n][m] != -1) return dp[n][m];
     if(a[n - 1] == b[m - 1]){
         int ans = lcs(a,n - 1,b,m - 1);
         return dp[n][m] = ans + 1;
     }
-    if(dp[n][m] != -1) return dp[n][m];
     else{
         int ans1 = lcs(a,n - 1,b,m);
         int ans2 = lcs(a,n,b,m - 1);
