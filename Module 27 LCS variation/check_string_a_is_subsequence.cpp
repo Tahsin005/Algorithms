e674208ae9fb76cda5a11dc3ea257e96de460586
Tahsin005 <tahsin.ferdous3546@gmail.com>
2023-09-13 01:58:45 +0600

    algo

---
+++ Module 27 LCS variation/check_string_a_is_subsequence.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define Tahsin ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+int main(){
+    Tahsin
+    string a,b;
+    cin>>a>>b;
+    int n = a.size();
+    int m = b.size();
+
+    // Normal O(N + M)
+
+    // int x = 0;
+    // bool ans = false;
+    // for(char c: b){
+    //     if(c == a[x]){
+    //         x++;
+    //     }
+    //     if(x == a.size()){
+    //         ans = true;
+    //         break;
+    //     }
+    // }
+    // if(ans) cout<<"YES"<<endl;
+    // else cout<<"NO"<<endl;
+
+
+
+    // Using LCS O(N * M)
+
+    int dp[n + 1][m + 1];
+    for(int i = 0; i <= n; i++){
+        for(int j = 0; j <= m; j++){
+            if(i == 0 or j == 0) dp[i][j] = 0;
+        }
+    }
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            if(a[i - 1] == b[j - 1]){
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            }
+            else{
+                dp[i][j] = max(dp[i - 1][j],dp[i][j - 1]);
+            }
+        }
+    }
+    int i = n,j = m;
+    string ans;
+    while(i != 0 and j != 0){
+        if(a[i - 1] == b[j - 1]){
+            ans += a[i - 1];
+            i--;
+            j--;
+        }
+        else{
+            if(dp[i][j - 1] > dp[i - 1][j]){
+                j--;
+            }
+            else{
+                i--;
+            }
+        }
+    }
+    reverse(ans.begin(),ans.end());
+    if(ans == a) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+    return 0;
+}
